44f207cc363dab4fbabfee7ec5db550ac610ff5d
fivemerrs <errgoubmehdifivemgithub@outlook.com>
2026-07-13 20:03:26 +0100

    ESX Legacy freeroam stack (ox_inventory, zones, shops)

---
+++ freeroam/resources/fr_hub/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'fr_hub'
+author 'fivemerrs'
+description 'Freeroam spawn hub NPCs (clothes + shop markers context)'
+version '1.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+}
+
+client_scripts {
+    'client.lua',
+}
+
+server_scripts {
+    'server.lua',
+}
