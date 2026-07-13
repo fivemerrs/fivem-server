5c76529cf5d2f794f4006a01d5e9248343aa7423
fivemerrs <errgoubmehdifivemgithub@outlook.com>
2026-07-13 20:03:24 +0100

    ESX Legacy freeroam stack (ox_inventory, zones, shops)

---
+++ freeroam/overlays/ox_inventory/data/shops.lua
@@ -0,0 +1,41 @@
+return {
+	FreeroamArmory = {
+		name = 'Freeroam Armory',
+		blip = {
+			id = 110, colour = 1, scale = 0.7
+		},
+		inventory = {
+			{ name = 'WEAPON_KNIFE', price = 200 },
+			{ name = 'WEAPON_BAT', price = 150 },
+			{ name = 'WEAPON_PISTOL', price = 1500 },
+			{ name = 'WEAPON_COMBATPISTOL', price = 2200 },
+			{ name = 'WEAPON_MICROSMG', price = 3500 },
+			{ name = 'WEAPON_CARBINERIFLE', price = 5000 },
+			{ name = 'ammo-9', price = 25 },
+			{ name = 'ammo-rifle', price = 40 },
+		},
+		locations = {
+			vec3(224.0, -858.5, 30.3),
+		},
+		targets = {},
+	},
+
+	FreeroamSupplies = {
+		name = 'Freeroam Supplies',
+		blip = {
+			id = 153, colour = 2, scale = 0.7
+		},
+		inventory = {
+			{ name = 'ammo-9', price = 25 },
+			{ name = 'ammo-rifle', price = 40 },
+			{ name = 'armour', price = 750 },
+			{ name = 'bandage', price = 50 },
+			{ name = 'burger', price = 25 },
+			{ name = 'water', price = 15 },
+		},
+		locations = {
+			vec3(228.5, -856.0, 30.3),
+		},
+		targets = {},
+	},
+}
