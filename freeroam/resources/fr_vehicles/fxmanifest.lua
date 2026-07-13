75cf6c00ee92e0d403c41c14058c9f658a759f4f
fivemerrs <errgoubmehdifivemgithub@outlook.com>
2026-07-13 21:46:26 +0100

    ESX Legacy freeroam stack (ox_inventory, zones, shops)

--- freeroam/resources/fr_vehicles/fxmanifest.lua
+++ freeroam/resources/fr_vehicles/fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 lua54 'yes'
 
 name 'fr_vehicles'
-description 'F3 spawn freeroam car (kanjo from user FF pack)'
+description 'F3 spawn Fast1 lore car (dukes / Menyoo presets)'
 version '1.0.0'
 
 shared_scripts {
