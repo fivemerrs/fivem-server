7723d07749eda9167d558342cebc96d539d2079b
fivemerrs <errgoubmehdifivemgithub@outlook.com>
2026-07-13 21:08:26 +0100

    ESX Legacy freeroam stack (ox_inventory, zones, shops)

---
+++ freeroam/resources/fr_vehicles/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'fr_vehicles'
+description 'F3 spawn freeroam car (kanjo from user FF pack)'
+version '1.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua',
+}
+
+client_script 'client.lua'
